68a5dad50ace66dd091679dd96a401880f5db6d0
Léolios <jean.christophe.henry.pro@gmail.com>
2020-03-23 23:28:53 +0000

    Delete sudo.

--- scripts/clean-ubuntu.sh
+++ scripts/clean-ubuntu.sh
@@ -19,32 +19,32 @@ echo "We aren't responsible for any brick or something else${normal}"
 echo "${bold}THIS PROGRAM USES SUPERUSER. BE CAREFUL !${normal}"
 read -p "${bold}Press any prompt to continue${normal}"
 echo "${bold}Updating packages list & tree from repositories source${normal}"
-sudo apt-get update -y
+apt-get update -y
 echo "${bold}Cleaning and removing unused packages${normal}"
-sudo apt-get autoclean -y
-sudo apt-get autoremove -y
+apt-get autoclean -y
+apt-get autoremove -y
 sleep 3
 echo "${bold}Updating packages${normal}"
-sudo apt-get dist-upgrade -y
+apt-get dist-upgrade -y
 sleep 3
 echo "${bold}Correcting dependecies${normal}"
-sudo apt-get -f -y install
+apt-get -f -y install
 sleep 3
 echo "${bold}Cleaning${normal}"
-sudo apt-get autoclean -y
+apt-get autoclean -y
 sleep 3
 echo "Cleaning Trash"
-sudo rm -r -f ~/.local/share/Trash/*/*
+rm -r -f ~/.local/share/Trash/*/*
 sleep 3
 echo "Cleaning Pictures"
 find ~/.thumbnails -type f -atime +7 -delete
 sleep 3
 echo "Run localpurge"
-sudo localepurge
+localepurge
 sleep 3
 echo "Clean source"
-sudo deborphan | xargs apt-get remove --purge -uyv && sudo aptitude purge $(dpkg --get-selections | grep deinstall | awk '{print $1}')
-sudo apt autoclean -y && sudo update-pciids -y && sudo update-usbids -y
+deborphan | xargs apt-get remove --purge -uyv && aptitude purge $(dpkg --get-selections | grep deinstall | awk '{print $1}')
+apt autoclean -y && update-pciids -y && update-usbids -y
 echo "${bold}Complete!${normal}"
 echo "${bold}--- THANKS FOR USING Clean ubuntu ---${normal}"
 read -p "${bold}Press any prompt to leave the program${normal}"
