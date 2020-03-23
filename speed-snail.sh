92e6cf9509cbbaa44abaea25bc63030ce92ecb47
Léolios <jean.christophe.henry.pro@gmail.com>
2020-03-23 04:02:55 +0000

    Update speed-snail.sh

--- speed-snail.sh
+++ speed-snail.sh
@@ -11,18 +11,17 @@ echo ""
 echo "@Autor : Jean-Christophe HENRY"
 echo "@email : jean.christophe.henry.pro@gmail.com"
 echo "@Git   : https://gitlab.com/jc.henry/speed-snail"
-echo "
-  Copyright (C) 2020
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-"
+echo ""
+echo "Copyright (C) 2020"
+echo "This program is free software; you can redistribute it and/or modify"
+echo "it under the terms of the GNU General Public License as published by"
+echo "the Free Software Foundation; either version 3 of the License, or"
+echo "(at your option) any later version."
+echo ""
+echo "This program is distributed in the hope that it will be useful,"
+echo "but WITHOUT ANY WARRANTY; without even the implied warranty of"
+echo "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+echo "GNU General Public License for more details."
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
 	if [[ $EUID -ne 0 ]]; then
@@ -96,18 +95,17 @@ if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
 	    echo "@Autor : Jean-Christophe HENRY"
 	    echo "@email : jean.christophe.henry.pro@gmail.com"
 	    echo "@Git   : https://gitlab.com/jc.henry/speed-snail"
-	    echo "
-		  Copyright (C) 2020
-	   	  This program is free software; you can redistribute it and/or modify
-	          it under the terms of the GNU General Public License as published by
-	          the Free Software Foundation; either version 3 of the License, or
-	          (at your option) any later version.
-
-	          This program is distributed in the hope that it will be useful,
-	          but WITHOUT ANY WARRANTY; without even the implied warranty of
-	          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	          GNU General Public License for more details.
-	    "
+	    echo ""
+	    echo "Copyright (C) 2020"
+	    echo "This program is free software; you can redistribute it and/or modify"
+	    echo "it under the terms of the GNU General Public License as published by"
+	    echo "the Free Software Foundation; either version 3 of the License, or"
+	    echo "(at your option) any later version."
+	    echo ""
+	    echo "This program is distributed in the hope that it will be useful,"
+	    echo "but WITHOUT ANY WARRANTY; without even the implied warranty of"
+	    echo "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+	    echo "GNU General Public License for more details."
 
 	    # loop for select instruction
 	    for choice in $choices
