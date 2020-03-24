a75849f987a8d3e4726b7705d932e6b78c74547f
Léolios <jean.christophe.henry.pro@gmail.com>
2020-03-24 12:44:49 +0000

    Update fix-bug-shadow.sh

--- scripts/fix-bug-shadow.sh
+++ scripts/fix-bug-shadow.sh
@@ -7,5 +7,5 @@ echo "##################"
 wget https://gitlab.com/aar642/libva-vdpau-driver/-/jobs/205841211/artifacts/raw/vdpau-va-driver_0.7.4-6ubuntu1_amd64.deb
 dpkg -i vdpau-va-driver_0.7.4-6ubuntu1_amd64.deb
 apt-mark hold vdpau-va-driver:amd64
-apt-get install vdpau-va-driver libvdpau1
+apt install vdpau-va-driver libvdpau1
 apt install mesa-va-drivers:i386 mesa-va-drivers
